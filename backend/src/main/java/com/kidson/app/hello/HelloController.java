f923a8c5851b783721ca0a828d87096ae4e33633
Studying_Jin <151742824+StudyingJin@users.noreply.github.com>
2025-11-01 04:12:45 +0900

    Chore/ci skeleton (#3)
    * ci: add minimal FE/BE workflows (status checks seed)
    
    * chore(repo): purge legacy app/* & IDE files; add skeleton
    
    * chore: FE/BE skeleton + CI/CD + ignore rules
    
    * chore: curate tracked files (FE/BE skeleton, CI, wrapper) and ignore env/build artifacts

---
+++ backend/src/main/java/com/kidson/app/hello/HelloController.java
@@ -0,0 +1,12 @@
+package com.kidson.app.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
