335782db466cb9c7b24d18b5180ada4597d87530
kimjin <kimjin5605@naver.com>
2025-11-04 00:55:00 +0900

    fix: correct aws ecr describe-images command syntax

--- backend/src/test/java/com/kidson/app/BackendApplicationTests.java
+++ backend/src/test/java/com/kidson/app/BackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.kidson.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.kidson.app.BackendApplication.class)
 class BackendApplicationTests {
 
 	@Test
--- backend/src/test/java/com/kidson/app/hello/HelloControllerTest.java
+++ backend/src/test/java/com/kidson/app/hello/HelloControllerTest.java
@@ -1,11 +1,12 @@
 package com.kidson.app.hello;
 
+import com.kidson.app.BackendApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(classes = BackendApplication.class)
 class HelloControllerTest {
 
     @Autowired
